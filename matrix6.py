0e819d41461252fc6c6cc9cd60a863579faf104d
PramodMane58 <pramodmane58@gmail.com>
2019-06-27 10:43:40 +0530

    problem dectionary,set and list

--- matrix6.py
+++ matrix6.py
@@ -1,30 +1,10 @@
-import numpy as np 
-a = np.array([5,2,6,2,7,5,6,8,2,9]) 
+Y = [[5,8,1],[6,7,3],[4,5,9]]
 
-print('First array:')
-print(a)
-print('\n')
+result =[]
+for i in range(len(Y[0])):
+    newlist = []
+    for j in range(len(Y)):
+        newlist.append(Y[j][i])
+    result.append(newlist)
 
-print 'Unique values of first array:' 
-u = np.unique(a) 
-
-u,indices = np.unique(a, return_index = True) 
-print(indices)
-
-print 'Indices of unique array:' 
-u,indices = np.unique(a,return_inverse = True) 
-print (indices)
-
-
-print 'Indices are:' 
-print indices 
-print '\n'  
-
-print 'Reconstruct the original array using indices:' 
-print u[indices] 
-print '\n'  
-
-print 'Return the count of repetitions of unique elements:' 
-u,indices = np.unique(a,return_counts = True) 
-print u 
-print indices
+print(result)
