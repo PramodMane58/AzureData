f86b587475953cc4ad6225ca07d1c034a3b0134d
PramodMane58 <pramodmane58@gmail.com>
2019-06-27 10:48:54 +0530

    problem dectionary,set and list

--- problem1.py
+++ problem1.py
@@ -1,8 +1,4 @@
 print('Please enter frist name and last name : ')
 name = input()
 namelist = name.split(' ')
-result = ''
-for n in range(len(namelist)-1,-1,-1):
-    result += namelist[n]
-    result += ' '
-print(result)
+print(" ".join(namelist[::-1]))
