26ff259b1dce9a832092fa9967a62a68d1ca8d11
PramodMane58 <pramodmane58@gmail.com>
2019-06-27 11:05:10 +0530

    problem dectionary,set and list

--- problem8.py
+++ problem8.py
@@ -1,13 +1,12 @@
 def histogram(*items):
     for n in items:
         output = ''
-        times = n
-        while( times > 0 ):
+        times = int(n)
+        while(times > 0 ):
           output += '*'
           times = times - 1
         print(output)
 
 print('Enter list of integers')
 list = input().split(',')
-print(list)
-#histogram(list)
+histogram(list)
